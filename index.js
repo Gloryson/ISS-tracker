2486db02d9d5db3f87d6dafa9f60c032e97d0b21
Vlad Bolkunov <gloryson@mail.ru>
2022-10-12 19:27:16 +0300

    feat: add script

---
+++ index.js
@@ -0,0 +1,13 @@
+let ISS = document.querySelector(`.moving__container`);
+
+let timer = setTimeout(function showISS () {
+  fetch('http://api.open-notify.org/iss-now.json')
+  .then(res => res.json())
+  .then(out => {
+          let latitude = out.iss_position.latitude;
+          let longitude = out.iss_position.longitude;
+          ISS.style.transform = `translateX(${50 / (180 / longitude)}%) translateY(${-40 / (90 / latitude)}%)`;
+        })
+  .catch(err => { throw err });
+  timer = setTimeout(showISS, 5000);
+}, 1000);
