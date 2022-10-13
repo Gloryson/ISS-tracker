824ddfe83b4431759ab6c98ef908a8aff633c4a9
Vlad Bolkunov <gloryson@mail.ru>
2022-10-13 07:41:43 +0300

    fix: change API URL

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let ISS = document.querySelector(`.moving__container`);
 
 let timer = setTimeout(function showISS () {
-  fetch('http://api.open-notify.org/iss-now.json')
-  .then(res => res.json())
+  fetch('https://api.wheretheiss.at/v1/satellites/25544')
+  .then(response => response.json())
   .then(out => {
-          let latitude = out.iss_position.latitude;
-          let longitude = out.iss_position.longitude;
+          let latitude = out.latitude;
+          let longitude = out.longitude;
           ISS.style.transform = `translateX(${50 / (180 / longitude)}%) translateY(${-40 / (90 / latitude)}%)`;
         })
   .catch(err => { throw err });
